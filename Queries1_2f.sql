55ed432dc19ace61e21ca1adfdfbd667e8147b07
Mehrshad Valizadeh Arjmand <mehrshad.valizadeh03@sharif.edu>
2025-05-02 00:04:54 +0330

    fix minor errors to make sure the code runs without any error

--- Queries1_2f.sql
+++ Queries1_2f.sql
@@ -15,7 +15,7 @@ create table team
 
 create table league_season
 (
-    season   smallint,
+    season      smallint,
     league_name varchar(30),
     start       date not null,
     finish      date not null,
@@ -35,200 +35,209 @@ create table team_in_season
     foreign key (league_name, season) references league_season (league_name, season)
 );
 
-CREATE type stadium_level as enum ('A' , 'B' , 'C') ;
+CREATE type stadium_level as enum ('A' , 'B' , 'C');
 
 create table stadium
 (
-    name varchar(30),
-    city varchar(10),
-    address text,
-    capacity smallint,
-    facilities text ,
-    level stadium_level ,
-    ticket_price int ,
+    name         varchar(30),
+    city         varchar(10),
+    address      text,
+    capacity     smallint,
+    facilities   text,
+    level        stadium_level,
+    ticket_price int,
     primary key (name)
 );
 
 create table week
 (
     league_name varchar(30),
-    season smallint,
-    number smallint,
-    start date ,
-    ending date ,
-    primary key (league_name , season , number) ,
-    foreign key (league_name , season) references league_season(league_name , season)
+    season      smallint,
+    number      smallint,
+    start       date,
+    ending      date,
+    primary key (league_name, season, number),
+    foreign key (league_name, season) references league_season (league_name, season)
 );
 
 create table game /* todo description */
 (
-    league_name varchar(30),
-    season smallint,
-    week_number smallint , /* todo */
-    number_in_week smallint ,
-    guest_id smallint ,
-    host_id smallint ,
-    stadium varchar(30) ,
-    guest_goal smallint ,
-    host_goal smallint ,
-    data date ,
-    time time ,
-    primary key (league_name , season , week_number , number_in_week) ,
-    foreign key (league_name , season , week_number) references week(league_name, season, number) ,
-    foreign key (guest_id) references team(id),
-    foreign key (host_id) references team(id), /*todo : summurize it ?*/
-    foreign key (stadium) references stadium(name) ,
-    unique(guest_id , host_id)
-) ;
+    league_name    varchar(30),
+    season         smallint,
+    week_number    smallint, /* todo */
+    number_in_week smallint,
+    guest_id       smallint,
+    host_id        smallint,
+    stadium        varchar(30),
+    guest_goal     smallint,
+    host_goal      smallint,
+    data           date,
+    time           time,
+    primary key (league_name, season, week_number, number_in_week),
+    foreign key (league_name, season, week_number) references week (league_name, season, number),
+    foreign key (guest_id) references team (id),
+    foreign key (host_id) references team (id), /*todo : summarize it ?*/
+    foreign key (stadium) references stadium (name),
+    unique (guest_id, host_id)
+);
 
-create type statistics as (
-    goals smallint ,
-    passes smallint ,
-    success_pass smallint ,
-    ball_possession numeric(4,2) ,
-    opportunities smallint ,
-    corners smallint ,
-    offside smallint ,
-    shoots smallint
-    );
-
-create table team_in_game /* todo week entity of two entity*/
+create type statistics as
 (
-    team_id smallint,
+    goals           smallint,
+    passes          smallint,
+    success_pass    smallint,
+    ball_possession numeric(4, 2),
+    opportunities   smallint,
+    corners         smallint,
+    offside         smallint,
+    shoots          smallint
+);
+
+create table team_in_game /* todo weak entity of two entity*/
+(
+    team_id     smallint,
     league_name varchar(30),
-    season smallint,
-    week_number smallint ,
-    game_id smallint,
-    statistics statistics ,
-    score smallint ,
-    primary key (team_id , league_name , season , week_number , game_id) ,
-    foreign key (team_id , league_name , season) references team_in_season(team_id , league_name , season) ,
-    foreign key (game_id , week_number) references game(number_in_week , week_number) ,
+    season      smallint,
+    week_number smallint,
+    game_id     smallint,
+    statistics  statistics,
+    score       smallint,
+    primary key (team_id, league_name, season, week_number, game_id),
+    foreign key (team_id, league_name, season) references team_in_season (team_id, league_name, season),
+    foreign key (league_name, season, week_number, game_id) references game (league_name, season, week_number, number_in_week)
     /* todo
     how check to this game even exists and how check that a team in season 5 do not playa a game in season 10 */
 );
 
-create type full_name as (
-    first_name varchar(10) ,
-    last_name varchar(15)
+create type full_name as
+(
+    first_name varchar(10),
+    last_name  varchar(15)
 );
-create table human (
-    english_name full_name ,
-    persian_name full_name ,
-    address text,
-    birthday date,
-    nationality varchar(15),
-    is_national_code boolean ,
-    id char(10),
+create table human
+(
+    english_name     full_name,
+    persian_name     full_name,
+    address          text,
+    birthday         date,
+    nationality      varchar(15),
+    is_national_code boolean,
+    id               char(10),
     primary key (id, is_national_code)
-) ;
+);
 
 create table player
 (
-    player_id smallint,
+    player_id         smallint,
     registration_date date,
-    human_id char(10),
-    is_national_code boolean,
+    human_id          char(10),
+    is_national_code  boolean,
     primary key (player_id),
-    foreign key (human_id, is_national_code) references human(id, is_national_code),
-    unique(human_id , is_national_code)
+    foreign key (human_id, is_national_code) references human (id, is_national_code),
+    unique (human_id, is_national_code)
 /* TODO: store free intervals) */
 );
 
 create table technical_personnel
 (
-    human_id char(10) ,
+    human_id         char(10),
     is_national_code boolean,
-    primary key (human_id , is_national_code)
-) ;
+    primary key (human_id, is_national_code)
+);
 
-create type side as enum('head_coach' , 'coach' , 'doctor' , 'supervisor' , 'others');
+create type side as enum ('head_coach' , 'coach' , 'doctor' , 'supervisor' , 'others');
 
 create table technical_sides
 (
-    personnel_id char(10) ,
-    is_national_code boolean ,
-    side side ,
-    primary key (personnel_id , is_national_code , side) ,
-    foreign key (personnel_id , is_national_code) references technical_personnel(human_id, is_national_code)
+    personnel_id     char(10),
+    is_national_code boolean,
+    side             side,
+    primary key (personnel_id, is_national_code, side),
+    foreign key (personnel_id, is_national_code) references technical_personnel (human_id, is_national_code)
 );
 
 /*change your aggregation*/
-create table technical_in_game(
-    personnel_id char(10) ,
-    is_national_code boolean ,
-    side side,
-    team_id smallint,
-    league_name varchar(30),
-    season smallint,
-    week_number smallint ,
-    game_id smallint,
-    primary key (personnel_id , is_national_code , side , team_id , league_name , season , week_number , game_id) ,
-    foreign key (personnel_id , is_national_code , side) references technical_sides(personnel_id, is_national_code, side) ,
-    foreign key (team_id , league_name , season , week_number , game_id) references
-        team_in_game(team_id, league_name, season, week_number, game_id)
+create table technical_in_game
+(
+    personnel_id     char(10),
+    is_national_code boolean,
+    side             side,
+    team_id          smallint,
+    league_name      varchar(30),
+    season           smallint,
+    week_number      smallint,
+    game_id          smallint,
+    primary key (personnel_id, is_national_code, side, team_id, league_name, season, week_number, game_id),
+    foreign key (personnel_id, is_national_code, side) references technical_sides (personnel_id, is_national_code, side),
+    foreign key (team_id, league_name, season, week_number, game_id) references
+        team_in_game (team_id, league_name, season, week_number, game_id)
 );
 
-create table foreign_team (
-    foreign_id smallint ,
-    country varchar(15) ,
-    name varchar(30),
+create table foreign_team
+(
+    foreign_id smallint,
+    country    varchar(15),
+    name       varchar(30),
     primary key (foreign_id)
 );
 
 /* !!!!!!!!!!!! players agreement */
 
-create table foreign_agreement(
-    player smallint ,
-    foreign_team smallint ,
-    league_name varchar(30),
-    season smallint,
-    native_team smallint,
-    price bigint ,
-    is_buying boolean , /* true if native team is buying*/
-    date date ,
-    primary key (is_buying , player , foreign_team , league_name , season , native_team) ,
-    foreign key (player) references player(player_id),
-    foreign key (foreign_team) references foreign_team(foreign_id),
-    foreign key (league_name , season , native_team) references team_in_season(league_name , season , team_id)
+create table foreign_agreement
+(
+    player       smallint,
+    foreign_team smallint,
+    league_name  varchar(30),
+    season       smallint,
+    native_team  smallint,
+    price        bigint,
+    is_buying    boolean, /* true if native team is buying*/
+    date         date,
+    primary key (is_buying, player, foreign_team, league_name, season, native_team),
+    foreign key (player) references player (player_id),
+    foreign key (foreign_team) references foreign_team (foreign_id),
+    foreign key (league_name, season, native_team) references team_in_season (league_name, season, team_id)
 );
 
-create table inner_agreement(
-    player smallint ,
-    seller smallint ,
+create table inner_agreement
+(
+    player      smallint,
+    seller      smallint,
     league_name varchar(30),
-    season smallint,
-    buyer smallint,
-    price bigint ,
-    date date ,
-    primary key (player , seller , league_name , season , buyer) ,
-    foreign key (player) references player(player_id),
-    foreign key (seller) references team_in_season(team_id),
-    foreign key (league_name , season , buyer) references team_in_season(league_name , season , team_id)
+    season      smallint,
+    buyer       smallint,
+    price       bigint,
+    date        date,
+    primary key (player, seller, league_name, season, buyer),
+    foreign key (player) references player (player_id),
+    foreign key (league_name, season, seller) references team_in_season (league_name, season, team_id),
+    foreign key (league_name, season, buyer) references team_in_season (league_name, season, team_id)
 );
 
-create table free_players_agreement(
-    player smallint ,
+create table free_players_agreement
+(
+    player      smallint,
     league_name varchar(30),
-    season smallint,
-    buyer smallint,
-    price bigint ,
-    date date ,
-    primary key (player , league_name , season , buyer) ,
-    foreign key (player) references player(player_id),
-    foreign key (league_name , season , buyer) references team_in_season(league_name , season , team_id)
+    season      smallint,
+    buyer       smallint,
+    price       bigint,
+    date        date,
+    primary key (player, league_name, season, buyer),
+    foreign key (player) references player (player_id),
+    foreign key (league_name, season, buyer) references team_in_season (league_name, season, team_id)
 );
 
-create table agreement_canceling(
-    player smallint,
+create table agreement_canceling
+(
+    player      smallint,
     league_name varchar(30),
-    season smallint,
-    buyer smallint,
-    cost bigint ,
-    date date ,
-    primary key (player , league_name , season , buyer) ,
-    foreign key (player) references player(player_id),
-    foreign key (league_name , season , buyer) references team_in_season(league_name , season , team_id)
+    season      smallint,
+    buyer       smallint,
+    cost        bigint,
+    date        date,
+    primary key (player, league_name, season, buyer),
+    foreign key (player) references player (player_id),
+    foreign key (league_name, season, buyer) references team_in_season (league_name, season, team_id)
 );
 /* todo : trades at first of leagues */
 /* end of trades */
@@ -241,29 +250,31 @@ create table referee
     level            smallint,
     primary key (id),
     foreign key (human_id, is_national_code) references human (id, is_national_code),
-    unique (human_id , is_national_code)
+    unique (human_id, is_national_code)
 );
 
-create table referee_team
+create table referee_team /* TODO: fix the ER diagram, and decide whether to merge this table into game table */
 (
-    league_name varchar(30),
-    season smallint,
-    week_number smallint , /* todo */
-    number_in_week smallint ,
-    main_referee smallint ,
-    first_assistant smallint ,
-    second_assistant smallint ,
-    fourth_referee smallint,
-    supervisor smallint ,
-    primary key (league_name , season , week_number , number_in_week) refrences
+    league_name      varchar(30),
+    season           smallint,
+    week_number      smallint, /* todo */
+    number_in_week   smallint,
+    main_referee     smallint,
+    first_assistant  smallint,
+    second_assistant smallint,
+    fourth_referee   smallint,
+    supervisor       smallint,
+    primary key (league_name, season, week_number, number_in_week),
+    foreign key (league_name, season, week_number, number_in_week) references game (league_name, season, week_number, number_in_week)
 );
 
 create type referee_position
 as enum ('main referee', 'first assistant referee', 'second assistant referee', 'supervisor');
 
-create table referee_in_league_committee
+create table referee_in_season_committee
 (
-    referee_id int references referee(id),
-    league_name varchar(30)
-/* TODO */
+    referee_id  int references referee (id) not null,
+    league_name varchar(30)                 not null,
+    season      smallint,
+    foreign key (league_name, season) references league_season (league_name, season)
 );
