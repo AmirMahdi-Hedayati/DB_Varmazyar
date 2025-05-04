34248da2e776f16bdcbe7483742acc48e1770528
AmirMahdi-Hedayati <hedayat>
2025-05-04 17:02:21 +0330

    remove todos

--- Queries1_2f.sql
+++ Queries1_2f.sql
@@ -60,11 +60,11 @@ create table week
     foreign key (league_name, season) references league_season (league_name, season)
 );
 
-create table game /* todo description */
+create table game
 (
     league_name    varchar(30),
     season         smallint,
-    week_number    smallint, /* todo */
+    week_number    smallint,
     number_in_week smallint,
     guest_id       smallint,
     host_id        smallint,
@@ -76,7 +76,7 @@ create table game /* todo description */
     primary key (league_name, season, week_number, number_in_week),
     foreign key (league_name, season, week_number) references week (league_name, season, number),
     foreign key (guest_id) references team (id),
-    foreign key (host_id) references team (id), /*todo : summarize it ?*/
+    foreign key (host_id) references team (id),
     foreign key (stadium) references stadium (name),
     unique (guest_id, host_id)
 );
@@ -93,7 +93,7 @@ create type statistics as
     shoots          smallint
 );
 
-create table team_in_game /* todo weak entity of two entity*/
+create table team_in_game
 (
     team_id     smallint,
     league_name varchar(30),
@@ -136,7 +136,7 @@ create table player
     primary key (player_id),
     foreign key (human_id, is_national_code) references human (id, is_national_code),
     unique (human_id, is_national_code)
-/* TODO: store free intervals) */
+
 );
 
 create table technical_personnel
@@ -241,7 +241,7 @@ create table agreement_canceling
     foreign key (league_name, season, buyer) references team_in_season (league_name, season, team_id)
 );
 
-/* todo : trades at first of leagues */
+
 create table trade_player_in_season
 (
     player      smallint       not null references player (player_id),
@@ -283,7 +283,7 @@ create table supervisor
     referee_id smallint primary key references referee (id)
 );
 
-create table referee_team /* TODO: fix the ER diagram, and decide whether to merge this table into game table */
+create table referee_team 
 (
     league_name      varchar(30),
     season           smallint,
